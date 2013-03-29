41342831ef5f194b4e08f1d9e27855bb78c966b2
Krapht <kraphteu@gmail.com>
2013-03-29 12:30:00 +0100

    Automated commit

--- src/krapht/modjam/TheBlockRenderer.java
+++ src/krapht/modjam/TheBlockRenderer.java
@@ -4,6 +4,7 @@ import net.minecraft.block.Block;
 import net.minecraft.client.renderer.RenderBlocks;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.IBlockAccess;
+import net.minecraftforge.common.ForgeDirection;
 import cpw.mods.fml.client.registry.ISimpleBlockRenderingHandler;
 
 public class TheBlockRenderer implements ISimpleBlockRenderingHandler {
@@ -19,11 +20,63 @@ public class TheBlockRenderer implements ISimpleBlockRenderingHandler {
 		TileEntity tile = world.getBlockTileEntity(x, y, z);
 		if (!(tile instanceof TheEntity)) return false;
 		
+		TheEntity theEntity = (TheEntity) tile;
 		
+		ForgeDirection dir = theEntity.getOrientation();
 		
+		switch(dir){
+		case DOWN:
+			renderer.uvRotateBottom 	= 0;
+			renderer.uvRotateTop 		= 0;
+			renderer.uvRotateNorth 		= 0;
+			renderer.uvRotateSouth 		= 0;
+			renderer.uvRotateWest 		= 0;
+			renderer.uvRotateEast 		= 0;
+			break;
+		case UP:
+			renderer.uvRotateBottom 	= 0;
+			renderer.uvRotateTop 		= 0;
+			renderer.uvRotateNorth 		= 2;
+			renderer.uvRotateSouth 		= 2;
+			renderer.uvRotateWest 		= 2;
+			renderer.uvRotateEast 		= 2;
+			break;
+		case NORTH:
+			renderer.uvRotateBottom 	= 0;
+			renderer.uvRotateTop 		= 0;
+			renderer.uvRotateNorth 		= 0;
+			renderer.uvRotateSouth 		= 0;
+			renderer.uvRotateWest 		= 0;
+			renderer.uvRotateEast 		= 0;
+			break;
+		case SOUTH:
+			renderer.uvRotateBottom 	= 0;
+			renderer.uvRotateTop 		= 0;
+			renderer.uvRotateNorth 		= 0;
+			renderer.uvRotateSouth 		= 0;
+			renderer.uvRotateWest 		= 0;
+			renderer.uvRotateEast 		= 0;
+			break;
+		case WEST:
+			renderer.uvRotateBottom 	= 0;
+			renderer.uvRotateTop 		= 0;
+			renderer.uvRotateNorth 		= 0;
+			renderer.uvRotateSouth 		= 0;
+			renderer.uvRotateWest 		= 0;
+			renderer.uvRotateEast 		= 0;
+			break;
+		case EAST:
+			renderer.uvRotateBottom 	= 0;
+			renderer.uvRotateTop 		= 0;
+			renderer.uvRotateNorth 		= 0;
+			renderer.uvRotateSouth 		= 0;
+			renderer.uvRotateWest 		= 0;
+			renderer.uvRotateEast 		= 0;
+			break;			
+		default:
+			break;
 		
-		renderer.uvRotateNorth = 3;
-		renderer.uvRotateTop = 1;
+		}
 		renderer.renderStandardBlock(block, x, y, z);
 		return false;
 	}
