2cc38f5215480e810dc20b18a841dea36a698ddf
Krapht <kraphteu@gmail.com>
2013-03-29 13:15:00 +0100

    Automated commit

--- src/krapht/modjam/TheBlockRenderer.java
+++ src/krapht/modjam/TheBlockRenderer.java
@@ -28,43 +28,43 @@ public class TheBlockRenderer implements ISimpleBlockRenderingHandler {
 		case DOWN:
 			renderer.uvRotateBottom 	= 0;
 			renderer.uvRotateTop 		= 0;
-			renderer.uvRotateNorth 		= 0;
-			renderer.uvRotateSouth 		= 0;
+			renderer.uvRotateNorth 		= 0;	//Actually West
+			renderer.uvRotateSouth 		= 0;	//Actually East
 			renderer.uvRotateWest 		= 0;
 			renderer.uvRotateEast 		= 0;
 			break;
 		case UP:
 			renderer.uvRotateBottom 	= 0;
 			renderer.uvRotateTop 		= 0;
-			renderer.uvRotateNorth 		= 3;
-			renderer.uvRotateSouth 		= 3;
+			renderer.uvRotateNorth 		= 3;	//Actually West
+			renderer.uvRotateSouth 		= 3;	//Actually East
 			renderer.uvRotateWest 		= 3;
 			renderer.uvRotateEast 		= 3;
 			break;
-//		case NORTH:
-//			renderer.uvRotateBottom 	= 3;
-//			renderer.uvRotateTop 		= 3;
-//			renderer.uvRotateNorth 		= 0;
-//			renderer.uvRotateSouth 		= 0;
-//			renderer.uvRotateWest 		= 1;
-//			renderer.uvRotateEast 		= 1;
-//			break;
-//		case SOUTH:
-//			renderer.uvRotateBottom 	= 0;
-//			renderer.uvRotateTop 		= 0;
-//			renderer.uvRotateNorth 		= 0;
-//			renderer.uvRotateSouth 		= 0;
-//			renderer.uvRotateWest 		= 0;
-//			renderer.uvRotateEast 		= 0;
-//			break;
-//		case WEST:
-//			renderer.uvRotateBottom 	= 2;
-//			renderer.uvRotateTop 		= 1;
-//			renderer.uvRotateNorth 		= 1;
-//			renderer.uvRotateSouth 		= 2;
-//			renderer.uvRotateWest 		= 0;
-//			renderer.uvRotateEast 		= 0;
-//			break;
+		case NORTH:
+			renderer.uvRotateBottom 	= 3;
+			renderer.uvRotateTop 		= 3;
+			renderer.uvRotateNorth 		= 1;	//Actually West
+			renderer.uvRotateSouth 		= 2;	//Actually East
+			renderer.uvRotateWest 		= 0;
+			renderer.uvRotateEast 		= 0;
+			break;
+		case SOUTH:
+			renderer.uvRotateBottom 	= 0;
+			renderer.uvRotateTop 		= 0;
+			renderer.uvRotateNorth 		= 2;	//Actually West
+			renderer.uvRotateSouth 		= 1;	//Actually East
+			renderer.uvRotateWest 		= 0;
+			renderer.uvRotateEast 		= 0;
+			break;
+		case WEST:
+			renderer.uvRotateBottom 	= 2;
+			renderer.uvRotateTop 		= 1;
+			renderer.uvRotateNorth 		= 0;	//Actually West
+			renderer.uvRotateSouth 		= 0;	//Actually East
+			renderer.uvRotateWest 		= 1;	//Actually South
+			renderer.uvRotateEast 		= 2;	//Actually North
+			break;
 //		case EAST:
 //			renderer.uvRotateBottom 	= 1;
 //			renderer.uvRotateTop 		= 2;
